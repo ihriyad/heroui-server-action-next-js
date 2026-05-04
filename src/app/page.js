ab352a08794e11314be2525c38d06941fa498c74
ihriyad <ihriyad.dev@gmail.com>
2026-05-04 11:06:18 +0600

    feat: add ThemeSwitch component with gravity-ui icons and update styles

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl text-center text-cyan-300 m-10">Hero Ui basic</h1>
+      <h1 className="text-4xl text-center m-10">Hero Ui basic</h1>
     </div>
   );
 }
